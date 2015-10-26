ebf6c156e0e396b8e4b5562bbd03aea9bfa1ef87
sreedhar4u <sreedhar4u@gmail.com>
2015-10-25 18:27:09 -0700

    search results are displayed
    search results are displayed under text box.

--- src/aura/ltngAutocomplete/ltngAutocompleteHelper.js
+++ src/aura/ltngAutocomplete/ltngAutocompleteHelper.js
@@ -5,17 +5,27 @@
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                console.log(response.getReturnValue());
+            	console.log(response.getReturnValue());
+                var returnedDataStrings = [];
+                var fieldsToDisplay = cmp.get("v.fetchFields");
+                for(var idx = 0 ; idx < response.getReturnValue().length; idx++){
+                	var rec = [];
+                	for(var idx1 = 0; idx1 < fieldsToDisplay.length; idx1++){
+                		var fieldName  = fieldsToDisplay[idx1];
+                		rec[idx1] = response.getReturnValue()[idx][fieldName];
+                	}
+                	returnedDataStrings[idx] =rec;
+                }
+                cmp.set("v.records", returnedDataStrings);
             }
             else if (state === "ERROR") {
                 var errors = response.getError();
                 if (errors) {
                     if (errors[0] && errors[0].message) {
-                        console.log("Error message: " + 
-                                 errors[0].message);
+                        alert("Error message: " + errors[0].message);
                     }
                 } else {
-                    console.log("Unknown error");
+                    alert("Unknown error");
                 }
             }
         });
